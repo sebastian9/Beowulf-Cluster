42335038250c442948b9e6290e74e024de86aa6a
Sebastian <sebastianls0228@gmail.com>
2019-04-12 16:24:34 +0000

    Change Redirectioning Back

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,8 +30,8 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to tasks_url, notice: 'Task was successfully created.' }
-        format.json { render :index, status: :created, location: tasks_url }
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
         format.json { render json: @task.errors, status: :unprocessable_entity }
